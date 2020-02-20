7de2ed07cc68f8c695fece263009538a6facbb66
binta diallo <bintadelen2009@gmail.com>
2020-02-20 15:32:35 -0800

    my own code

---
+++ Myown.code.js
@@ -0,0 +1,20 @@
+var name = "Bob";
+var isRaining = true;
+var hasPet = false;
+var job = "Web Developer";
+var cash = 100;
+
+if (isRaining){
+    cash -= 50;
+    isRaining = false
+    console.log("I need a coat");
+//console.log(cash);
+}
+if (name != "Tom"){
+    console.log("Hello Stranger");
+}
+if (job = "Web Developer"){
+    cash += 500;
+    console.log("Keep up the good work");
+console.log(cash);
+}
